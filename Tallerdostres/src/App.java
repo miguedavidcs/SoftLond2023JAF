30ca9bc0c4851e3b1ebfc331ddf91c1ba0efc0eb
migueldavidcs <midacasa17@gmail.com>
2023-08-09 10:03:34 -0500

    cambios ultima hora

--- Tallerdostres/src/App.java
+++ Tallerdostres/src/App.java
@@ -1,18 +1,30 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import com.softlond.opereacion.Operacion;
+
 public class App {
-    
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        com.softlond.opereacion.Operacion operacion = new com.softlond.opereacion.Operacion();
 
-        System.out.print("Ingrese una expresión matemática: ");
-        String expresion = scanner.nextLine();
+        try {
+            System.out.print("Ingrese el primer número: ");
+            double n1 = scanner.nextDouble();
 
-        double resultado = operacion.realizarCalculo(expresion);
-        System.out.println("El resultado es: " + resultado);
+            System.out.print("Ingrese el operador (+, -, *, /): ");
+            char simbolo = scanner.next().charAt(0);
+
+            System.out.print("Ingrese el segundo número: ");
+            double n2 = scanner.nextDouble();
+
+            Operacion calculadora = new Operacion();
+            double resultado = calculadora.realizarOperacion(n1, simbolo, n2);
+
+            System.out.println("El resultado es: " + resultado);
+        } catch (InputMismatchException e) {
+            System.out.println("Entrada inválida. Asegúrese de ingresar números y operadores válidos.");
+        }
 
         scanner.close();
-
     }
 }
--- Tallerdostres/src/com/softlond/opereacion/Operacion.java
+++ Tallerdostres/src/com/softlond/opereacion/Operacion.java
@@ -1,147 +1,32 @@
 package com.softlond.opereacion;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
-import java.util.Scanner;
-
 public class Operacion {
-    public double realizarCalculo(String expresion) {
-        return evaluarExpresion(expresion);
-    }
-
-    private double evaluarExpresion(String expresion) {
-        expresion = expresion.replaceAll(" ", "");
-
-        while (expresion.contains("(")) {
-            int indexAbre = expresion.lastIndexOf("(");
-            int indexCierra = expresion.indexOf(")", indexAbre);
-
-            String subExpresion = expresion.substring(indexAbre + 1, indexCierra);
-            double resultadoSubExpresion = evaluarExpresion(subExpresion);
-
-            expresion = expresion.replace("(" + subExpresion + ")", String.valueOf(resultadoSubExpresion));
-        }
-
-        while (expresion.contains("*") || expresion.contains("/")) {
-            int indexMultiplicacion = expresion.indexOf("*");
-            int indexDivision = expresion.indexOf("/");
-
-            int indexOperador = Math.min(indexMultiplicacion, indexDivision);
-
-            String subExpresion = obtenerSubExpresion(expresion, indexOperador);
-            double resultadoSubExpresion = evaluarSubExpresion(subExpresion);
-
-            expresion = reemplazarSubExpresion(expresion, subExpresion, String.valueOf(resultadoSubExpresion));
-        }
-
-        return evaluarOperaciones(expresion);
-    }
-
-    private String obtenerSubExpresion(String expresion, int indexOperador) {
-        int inicioSubExpresion = indexOperador;
-        while (inicioSubExpresion > 0 && esOperador(expresion.charAt(inicioSubExpresion - 1))) {
-            inicioSubExpresion--;
-        }
-
-        int finSubExpresion = indexOperador + 1;
-        while (finSubExpresion < expresion.length() && esOperador(expresion.charAt(finSubExpresion))) {
-            finSubExpresion++;
-        }
-
-        return expresion.substring(inicioSubExpresion, finSubExpresion);
-    }
-
-    private boolean esOperador(char caracter) {
-        return caracter == '+' || caracter == '-' || caracter == '*' || caracter == '/';
-    }
-
-    private double evaluarSubExpresion(String subExpresion) {
-        Deque<Double> numeros = new ArrayDeque<>();
-        Deque<Character> operadores = new ArrayDeque<>();
-
-        try (Scanner scanner = new Scanner(subExpresion).useDelimiter("\\s*")) {
-            while (scanner.hasNext()) {
-                if (scanner.hasNextDouble()) {
-                    numeros.push(scanner.nextDouble());
-                } else {
-                    char operador = scanner.next().charAt(0);
-                    while (!operadores.isEmpty() && getPrecedencia(operador) <= getPrecedencia(operadores.peek())) {
-                        aplicarOperador(numeros, operadores.pop());
-                    }
-                    operadores.push(operador);
-                }
-            }
-
-            while (!operadores.isEmpty()) {
-                aplicarOperador(numeros, operadores.pop());
-            }
-
-            scanner.close();
-        }
-
-        return numeros.pop();
-    }
-
-    private int getPrecedencia(char operador) {
-        if (operador == '+' || operador == '-') {
-            return 1;
-        } else if (operador == '*' || operador == '/') {
-            return 2;
-        }
-        return 0;
-    }
-
-    private void aplicarOperador(Deque<Double> numeros, char operador) {
-        double numero2 = numeros.pop();
-        double numero1 = numeros.pop();
+    public double realizarOperacion(double num1, char operador, double num2) {
         double resultado = 0;
 
         switch (operador) {
             case '+':
-                resultado = numero1 + numero2;
+                resultado = num1 + num2;
                 break;
             case '-':
-                resultado = numero1 - numero2;
+                resultado = num1 - num2;
                 break;
             case '*':
-                resultado = numero1 * numero2;
+                resultado = num1 * num2;
                 break;
             case '/':
-                resultado = numero1 / numero2;
-                break;
-        }
-
-        numeros.push(resultado);
-    }
-
-    private String reemplazarSubExpresion(String expresion, String subExpresion, String resultado) {
-        return expresion.replace(subExpresion, resultado);
-    }
-
-    private double evaluarOperaciones(String expresion) {
-        Deque<Double> numeros = new ArrayDeque<>();
-        Deque<Character> operadores = new ArrayDeque<>();
-
-        try (Scanner scanner = new Scanner(expresion).useDelimiter("\\s*")) {
-            while (scanner.hasNext()) {
-                if (scanner.hasNextDouble()) {
-                    numeros.push(scanner.nextDouble());
+                if (num2 != 0) {
+                    resultado = num1 / num2;
                 } else {
-                    char operador = scanner.next().charAt(0);
-                    while (!operadores.isEmpty() && getPrecedencia(operador) <= getPrecedencia(operadores.peek())) {
-                        aplicarOperador(numeros, operadores.pop());
-                    }
-                    operadores.push(operador);
+                    System.out.println("No se puede dividir por cero.");
+                    System.exit(0);
                 }
-            }
-
-            while (!operadores.isEmpty()) {
-                aplicarOperador(numeros, operadores.pop());
-            }
-
-            scanner.close();
+                break;
+            default:
+                System.out.println("Operador no válido.");
+                System.exit(0);
         }
 
-        return numeros.pop();
+        return resultado;
     }
 }
